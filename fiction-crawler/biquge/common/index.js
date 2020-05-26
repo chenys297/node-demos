ad858871de51ded43e135109f0ea509e6bb2f6ba
lip297 <lip.smith297@gmail.com>
2020-05-26 23:58:16 +0800

    暂存代码

--- fiction-crawler/biquge/common/index.js
+++ fiction-crawler/biquge/common/index.js
@@ -22,3 +22,130 @@ exports.getFictionHomeUrl = function (strSearchResHtml, fictionName) {
   }
   return url;
 };
+
+/*
+searchResList[i] = 
+{
+  type: 'tag',
+  name: 'span',
+  namespace: 'http://www.w3.org/1999/xhtml',
+  attribs: [Object: null prototype] { class: 's2' },
+  'x-attribsNamespace': [Object: null prototype] { class: undefined },
+  'x-attribsPrefix': [Object: null prototype] { class: undefined },
+  children: [
+    {
+      type: 'tag',
+      name: 'b',
+      namespace: 'http://www.w3.org/1999/xhtml',
+      attribs: [Object: null prototype] {},
+      'x-attribsNamespace': [Object: null prototype] {},
+      'x-attribsPrefix': [Object: null prototype] {},
+      children: [Array],
+      parent: [Circular],
+      prev: null,
+      next: null
+    }
+  ],
+  parent: {
+    type: 'tag',
+    name: 'li',
+    namespace: 'http://www.w3.org/1999/xhtml',
+    attribs: [Object: null prototype] {},
+    'x-attribsNamespace': [Object: null prototype] {},
+    'x-attribsPrefix': [Object: null prototype] {},
+    children: [
+      [Object],   [Object],
+      [Circular], [Object],
+      [Object],   [Object],
+      [Object],   [Object],
+      [Object],   [Object],
+      [Object],   [Object],
+      [Object],   [Object]
+    ],
+    parent: {
+      type: 'tag',
+      name: 'ul',
+      namespace: 'http://www.w3.org/1999/xhtml',
+      attribs: [Object: null prototype] {},
+      'x-attribsNamespace': [Object: null prototype] {},
+      'x-attribsPrefix': [Object: null prototype] {},
+      children: [Array],
+      parent: [Object],
+      prev: [Object],
+      next: [Object]
+    },
+    prev: {
+      type: 'text',
+      data: '\n                ',
+      parent: [Object],
+      prev: null,
+      next: [Circular]
+    },
+    next: {
+      type: 'text',
+      data: '\n\n                                    ',
+      parent: [Object],
+      prev: [Circular],
+      next: [Object]
+    }
+  },
+  prev: {
+    type: 'text',
+    data: '\n                    ',
+    parent: {
+      type: 'tag',
+      name: 'li',
+      namespace: 'http://www.w3.org/1999/xhtml',
+      attribs: [Object: null prototype] {},
+      'x-attribsNamespace': [Object: null prototype] {},
+      'x-attribsPrefix': [Object: null prototype] {},
+      children: [Array],
+      parent: [Object],
+      prev: [Object],
+      next: [Object]
+    },
+    prev: {
+      type: 'tag',
+      name: 'span',
+      namespace: 'http://www.w3.org/1999/xhtml',
+      attribs: [Object: null prototype],
+      'x-attribsNamespace': [Object: null prototype],
+      'x-attribsPrefix': [Object: null prototype],
+      children: [Array],
+      parent: [Object],
+      prev: null,
+      next: [Circular]
+    },
+    next: [Circular]
+  },
+  next: {
+    type: 'text',
+    data: '\n                    ',
+    parent: {
+      type: 'tag',
+      name: 'li',
+      namespace: 'http://www.w3.org/1999/xhtml',
+      attribs: [Object: null prototype] {},
+      'x-attribsNamespace': [Object: null prototype] {},
+      'x-attribsPrefix': [Object: null prototype] {},
+      children: [Array],
+      parent: [Object],
+      prev: [Object],
+      next: [Object]
+    },
+    prev: [Circular],
+    next: {
+      type: 'tag',
+      name: 'span',
+      namespace: 'http://www.w3.org/1999/xhtml',
+      attribs: [Object: null prototype],
+      'x-attribsNamespace': [Object: null prototype],
+      'x-attribsPrefix': [Object: null prototype],
+      children: [Array],
+      parent: [Object],
+      prev: [Circular],
+      next: [Object]
+    }
+  }
+}
+*/
